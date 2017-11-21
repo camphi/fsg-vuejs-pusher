5c48a3c8894e43d4dfe66bb2ab2d7e3556c7786b
vagrant <vagrant@homestead>
2017-11-21 02:55:52 +0000

    Initial commit

---
+++ database/seeds/NotificationsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class NotificationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('songs')->truncate();
+        $faker = Faker::create();
+
+        foreach (range(1,10) as $index) {
+            DB::table('notifications')->insert([
+                'from' => $faker->name,
+                'message' => $faker->paragraph,
+            ]);
+        }
+
+    }
+}
